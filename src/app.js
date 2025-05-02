98a7f1cc30dad13fd1520cad29c02de514bdd16f
Sam <samuelgkeller@gmail.com>
2025-05-02 16:38:21 -0400

    Slight changes for clarity

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+const name = 'katsuyou-api';
+
+const express = require('express');
+const app = express();
+const port = 7825;
+
 require('dotenv').config(); // imports dotenv library
 const mongoose = require('mongoose'); // imports mongoose to allow access to db
 const mongoURI = process.env.MONGO_URI;  // grabs MONGO_URI value from .env and stores it in a variable
@@ -7,16 +13,10 @@ mongoose.connect(mongoURI).then(() => {
     console.error('MongoDB connection error:', error);
 })
 
-const express = require('express');
-const app = express();
-const port = 7825;
-
 app.get('/api/test', (req, res) => {
-    res.send('hello');
+    res.status(200).json({message:`Test successful. Successfully calling from ${name}`});
 });
 
-
-
 app.listen(port, () => {
-    console.log(`katsuyou-api running... PORT: ${port}`)
+    console.log(`${name} running... PORT: ${port}`)
 });
