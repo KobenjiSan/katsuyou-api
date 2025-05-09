1527ae6e72e680b1844e3af0b1031a35c359f988
Sam <samuelgkeller@gmail.com>
2025-05-09 15:09:19 -0400

    Installed Morgan to log incomming requests, added stricter validations in controller.getVerb

--- src/app.js
+++ src/app.js
@@ -4,13 +4,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 7825;
 
+const morgan = require('morgan');
+
 const katsuyouRouter = require('./routes/katsuyou.routes');
 
 const { connectMongo } = require('./utils/connectMongo');
 
 connectMongo()
     .then(() => {
+        app.use(morgan('dev')); // logs incoming requests
+
         app.use('/api/katsuyou', katsuyouRouter);
+
+        // Global error handler (fallback if something isnt caught properly)
+        app.use((err, req, res, next) => {
+            console.error(err.stack);
+            res.status(500).json({message: 'Something went wrong.'});
+        });
 
         app.listen(port, () => {
             console.log(`${name} running... PORT: ${port}`)
--- src/controllers/katsuyou.controller.js
+++ src/controllers/katsuyou.controller.js
@@ -2,20 +2,25 @@ const Verb = require('../models/verbs'); // Loads the file that defines Mongoose
 
 const getVerb = async (req, res) => {
     try{
-        let data = await Verb.findOne({hiragana: req.params.word});
-
-        if(data === null){
-            data = await Verb.findOne({kanji: req.params.word});
-        }
-
-        if(data === null){
-            res.status(400).json({error: `${req.params.word} is not in our database`});
+        const regCheck = /^[\u3040-\u30FF\u4E00-\u9FFF\uFF66-\uFF9D]+$/;
+        if(!req.params.word || req.params.word.trim() === '' || !regCheck.test(req.params.word)){
+            return res.status(400).json({error: `${req.params.word} is not a valid entry`});
         }else{
-            res.status(200).json(data);
+            let data = await Verb.findOne({hiragana: req.params.word});
+
+            if(data === null){
+                data = await Verb.findOne({kanji: req.params.word});
+            }
+
+            if(data === null){
+                return res.status(400).json({error: `${req.params.word} is not in our database`});
+            }else{
+                return res.status(200).json(data);
+            }
         }
     }catch(error){
         console.error('MongoDB ERROR:', error);
-        res.status(500).json({ message: 'Internal Server Error', error: error.message });
+        return res.status(500).json({error: error.message });
     }
 }
 
