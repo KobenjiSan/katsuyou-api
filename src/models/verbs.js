ed1d9c8d3188382c0bffb6a79be78ad057068a55
Sam <samuelgkeller@gmail.com>
2025-05-09 09:55:54 -0400

    Added verbs.json and placeholder for seeding script

--- src/models/verbs.js
+++ src/models/verbs.js
@@ -3,12 +3,8 @@ const mongoose = require('mongoose');
 const verbSchema = new mongoose.Schema({
     hiragana: { type: String, required: true },
     kanji: { type: String, required: true },
-    meaning: { type: String, required: true },  // could become: meanings: [{ meaing: String , pastMeaning: String }]
-    pastMeaning: { type: String, required: false }, // false in case I cant find a good past-tense translation 
+    meaning: { type: String, required: true },
     type: { type: String, enum:['ichidan', 'godan', 'irregular'], required: true},
-    // false as I might build the word then add sentence later, 
-    // also might need to be an object for words with multiple meanings
-    // could become: examples: [{ template: String, meaning: String}]
     exampleTemplate: { type: String, required: false }, 
     exampleMeaning: { type: String, required: false }
 });
